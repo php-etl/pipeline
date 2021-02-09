bc3b242ba4ae082e69b37b32899ada45c5a3d8b0
Grégory Planchat <gregory@kiboko.fr>
2021-02-09 21:30:50 +0100

    Changed pipeline runner in order to remove some complexity

--- src/PipelineRunner.php
+++ src/PipelineRunner.php
@@ -27,56 +27,53 @@ class PipelineRunner implements PipelineRunnerInterface
      */
     public function run(\Iterator $source, \Generator $coroutine): \Iterator
     {
-        return new ResumableIterator(function (\Iterator $source) use ($coroutine) {
-            $wrapper = new GeneratorWrapper();
-            $wrapper->rewind($source, $coroutine);
+        $wrapper = new GeneratorWrapper();
+        $wrapper->rewind($source, $coroutine);
 
-            while ($wrapper->valid($source)) {
-                $bucket = $coroutine->send($source->current());
+        while ($wrapper->valid($source)) {
+            $bucket = $coroutine->send($source->current());
 
-                if ($bucket === null) {
-                    break;
-                }
-
-                if (!$bucket instanceof ResultBucketInterface) {
-                    throw UnexpectedYieldedValueType::expectingTypes(
-                        $coroutine,
-                        [ResultBucketInterface::class],
-                        $bucket
-                    );
-                }
-
-                if ($bucket instanceof RejectionResultBucketInterface) {
-                    foreach ($bucket as $rejection) {
-                        $this->logger->log(
-                            $this->rejectionLevel,
-                            'Some data was rejected from the pipeline',
-                            [
-                                'line' => $rejection
-                            ]
-                        );
-                    }
-                }
-
-                if (!$bucket instanceof AcceptanceResultBucketInterface
-                    && !$bucket instanceof RejectionResultBucketInterface
-                ) {
-                    throw UnexpectedYieldedValueType::expectingTypes(
-                        $coroutine,
-                        [
-                            AcceptanceResultBucketInterface::class,
-                            RejectionResultBucketInterface::class,
-                        ],
-                        $bucket
-                    );
-                }
-
-                if ($bucket instanceof AcceptanceResultBucketInterface) {
-                    yield from $bucket->walkAcceptance();
-                }
-
-                $wrapper->next($source, $coroutine);
+            if ($bucket === null) {
+                break;
             }
-        }, $source);
+
+            if (!$bucket instanceof ResultBucketInterface) {
+                throw UnexpectedYieldedValueType::expectingTypes(
+                    $coroutine,
+                    [ResultBucketInterface::class],
+                    $bucket
+                );
+            }
+
+            if ($bucket instanceof RejectionResultBucketInterface) {
+                foreach ($bucket as $rejection) {
+                    $this->logger->log(
+                        $this->rejectionLevel,
+                        'Some data was rejected from the pipeline',
+                        [
+                            'line' => $rejection
+                        ]
+                    );
+                }
+            }
+
+            if (!$bucket instanceof ResultBucketInterface) {
+                throw UnexpectedYieldedValueType::expectingTypes(
+                    $coroutine,
+                    [
+                        ResultBucketInterface::class,
+                        AcceptanceResultBucketInterface::class,
+                        RejectionResultBucketInterface::class,
+                    ],
+                    $bucket
+                );
+            }
+
+            if ($bucket instanceof AcceptanceResultBucketInterface) {
+                yield from $bucket->walkAcceptance();
+            }
+
+            $wrapper->next($source);
+        }
     }
 }
---
+++ unit/PipelineRunnerTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace unit\Kiboko\Component\ETL\Pipeline;
+
+use Kiboko\Component\Bucket\AcceptanceResultBucket;
+use Kiboko\Component\Bucket\EmptyResultBucket;
+use Kiboko\Component\Pipeline\PipelineRunner;
+use Psr\Log\NullLogger;
+
+class PipelineRunnerTest extends IterableTestCase
+{
+    public function providerRun()
+    {
+        // Test if pipeline can walk items, without adding or removing any item
+        yield [
+            new \ArrayIterator([
+                'lorem',
+                'ipsum',
+                'dolor',
+                'sit',
+                'amet',
+                'consecutir',
+            ]),
+            function() {
+                $item = yield;
+                while (true) {
+                    $item = yield new AcceptanceResultBucket(strrev($item));
+                }
+            },
+            [
+                'merol',
+                'muspi',
+                'rolod',
+                'tis',
+                'tema',
+                'ritucesnoc',
+            ]
+        ];
+
+        // Test if pipeline can walk items, while removing some items
+        yield [
+            new \ArrayIterator([
+                'lorem',
+                'ipsum',
+                'dolor',
+                'sit',
+                'amet',
+                'consecutir',
+            ]),
+            function() {
+                $item = yield;
+                while (true) {
+                    static $i = 0;
+                    if ($i++ % 2 === 0) {
+                        $item = yield new AcceptanceResultBucket(strrev($item));
+                    } else {
+                        $item = yield new EmptyResultBucket();
+                    }
+                }
+            },
+            [
+                'merol',
+                'rolod',
+                'tema',
+            ]
+        ];
+
+        // Test if pipeline can walk items, while adding some items
+        yield [
+            new \ArrayIterator([
+                'lorem',
+                'ipsum',
+                'dolor',
+                'sit',
+                'amet',
+                'consecutir',
+            ]),
+            function() {
+                $item = yield;
+                while (true) {
+                    $item = yield new AcceptanceResultBucket(
+                        $item,
+                        strrev($item)
+                    );
+                }
+            },
+            [
+                'lorem',
+                'merol',
+                'ipsum',
+                'muspi',
+                'dolor',
+                'rolod',
+                'sit',
+                'tis',
+                'amet',
+                'tema',
+                'consecutir',
+                'ritucesnoc',
+            ]
+        ];
+    }
+
+    /**
+     * @param \Iterator $source
+     * @param callable  $callback
+     * @param array     $expected
+     *
+     * @dataProvider providerRun
+     */
+    public function testRun(\Iterator $source, callable $callback, array $expected)
+    {
+        $run = new PipelineRunner(new NullLogger());
+
+        $it = $run->run($source, $callback());
+
+        $this->assertIteration(new \ArrayIterator($expected), $it);
+    }
+}
