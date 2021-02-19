6f550d900a1583baf65868777d7a0f27e7c81735
Grégory Planchat <gregory@kiboko.fr>
2021-02-19 10:19:03 +0100

    Committed missing test class

---
+++ unit/IterableTestCase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace unit\Kiboko\Component\ETL\Pipeline;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class IterableTestCase extends TestCase
+{
+    protected function assertIteration(\Traversable $expected, \Traversable $actual, $message = '')
+    {
+        if ($message) {
+            $message .= "\n";
+        }
+
+        $expected = $expected instanceof \Iterator ? $expected : new \IteratorIterator($expected);
+        $actual   = $actual instanceof \Iterator ? $actual : new \IteratorIterator($actual);
+
+        $both = new \MultipleIterator(\MultipleIterator::MIT_NEED_ANY);
+        $both->attachIterator($expected);
+        $both->attachIterator($actual);
+
+        $index = 0;
+        foreach ($both as list($expectedItem, $actualItem)) {
+            ++$index;
+            //$this->assertTrue($actual->valid(), sprintf("%sActual iterator is closed, some items are missing after index #%d", $message, $index));
+            //$this->assertTrue($expected->valid(), sprintf("%sExpected iterator is closed, some items are in excess after index #%d", $message, $index));
+            $this->assertSame($expectedItem, $actualItem, sprintf("%sValues of Iteration #%d", $message, $index));
+        }
+    }
+}
